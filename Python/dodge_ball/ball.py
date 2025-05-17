4a838ed62622ad7407b03f1fef26e795bc4dffb3
Woojin <koving11@naver.com>
2025-05-18 03:15:35 +0900

    Apply oiia cat image as ball and clean pycache

--- Python/dodge_ball/ball.py
+++ Python/dodge_ball/ball.py
@@ -2,14 +2,17 @@
 
 import pygame
 import random
-from config import BALL_RADIUS, RED, WIDTH
+from config import BALL_RADIUS, WIDTH
+import os
 
 class Ball:
     def __init__(self, speed):
         self.x = random.randint(BALL_RADIUS, WIDTH - BALL_RADIUS)
         self.y = -BALL_RADIUS
-        self.radius = BALL_RADIUS
         self.speed = speed
+        image_path = os.path.join("images", "oiia_cat.gif")
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (BALL_RADIUS * 2, BALL_RADIUS * 2))
 
     def update(self):
         self.y += self.speed
@@ -18,7 +21,14 @@ class Ball:
         return self.y > height
 
     def get_rect(self):
-        return pygame.Rect(self.x - self.radius, self.y - self.radius, self.radius * 2, self.radius * 2)
+        offset_x = 6  # 이미지 바깥쪽 여백
+        offset_y = 5
+        width = self.image.get_width() - 2 * offset_x
+        height = self.image.get_height() - 2 * offset_y
+        return pygame.Rect(self.x + offset_x, self.y + offset_y, width, height)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, RED, (self.x, self.y), self.radius)
+        screen.blit(self.image, (self.x, self.y))
+        # 충돌범위 확인
+        pygame.draw.rect(screen, (0, 255, 0), self.get_rect(), 2)  # 녹색 충돌 박스 그리기
+
